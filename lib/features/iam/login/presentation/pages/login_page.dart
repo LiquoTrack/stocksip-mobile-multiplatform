d76f34cb2e6ee65ad7ad29bd20ce0714fec5fd55
Far14z <faridce14@gmail.com>
2025-11-12 15:27:37 -0500

    feat(login): enhance login page ui with logo, improved input fields, and layout adjustments.

--- lib/features/iam/login/presentation/pages/login_page.dart
+++ lib/features/iam/login/presentation/pages/login_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stocksip/features/iam/login/presentation/blocs/login_bloc.dart';
@@ -32,58 +31,178 @@ class _LoginPageState extends State<LoginPage> {
       },
       child: Stack(
         children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: MediaQuery.of(context).size.height * 0.4,
+            child: Container(color: const Color(0xFF2B000D)),
+          ),
+
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.6,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(color: const Color(0xFFF4ECEC)),
+          ),
+
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: TextField(
-                  onChanged: (value) => context.read<LoginBloc>().add(
-                    OnEmailChanged(email: value),
-                  ),
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Email',
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: TextField(
-                  onChanged: (value) => context.read<LoginBloc>().add(
-                    OnPasswordChanged(password: value),
-                  ),
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Password',
-                    suffixIcon: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          _isHidden = !_isHidden;
-                        });
-                      },
-                      icon: Icon(
-                        _isHidden ? Icons.visibility_off : Icons.visibility,
+              SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image(
+                        image: AssetImage('assets/images/stocksip_logo.png'),
+                        height: MediaQuery.of(context).size.height * 0.15,
                       ),
                     ),
-                  ),
-                  obscureText: _isHidden,
+                  ],
                 ),
               ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Stock',
+                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                      color: Colors.red,
+                      fontSize: 64,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Sip',
+                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                      color: Colors.white,
+                      fontSize: 64,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: SizedBox(
-                  height: 48,
-                  width: double.infinity,
-                  child: FilledButton(
-                    onPressed: () => context.read<LoginBloc>().add(Login()),
-                    child: Text('Sign in'),
+                  width: MediaQuery.of(context).size.width * 0.85,
+                  child: TextField(
+                    onChanged: (value) => context.read<LoginBloc>().add(
+                      OnEmailChanged(email: value),
+                    ),
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(),
+                      hintText: 'Email',
+                      prefixIcon: const Icon(Icons.email),
+                      filled: true,
+                      fillColor: Color.fromRGBO(255, 255, 255, 0.9),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: BorderSide(color: Colors.transparent),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: BorderSide(color: Colors.transparent),
+                      ),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
                   ),
                 ),
               ),
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.85,
+                  child: TextField(
+                    onChanged: (value) => context.read<LoginBloc>().add(
+                      OnPasswordChanged(password: value),
+                    ),
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(),
+                      prefixIcon: const Icon(Icons.lock),
+                      hintText: 'Password',
+                      filled: true,
+                      fillColor: Color.fromRGBO(255, 255, 255, 0.9),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _isHidden = !_isHidden;
+                          });
+                        },
+                        icon: Icon(
+                          _isHidden ? Icons.visibility_off : Icons.visibility,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: BorderSide(color: Colors.transparent),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: BorderSide(color: Colors.transparent),
+                      ),
+                    ),
+                    obscureText: _isHidden,
+                  ),
+                ),
+              ),
+
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Forgot Password?',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.width * 0.15,
+                  width: MediaQuery.of(context).size.width * 0.85,
+                  child: FilledButton(
+                    style: FilledButton.styleFrom(
+                      backgroundColor: const Color(0xFF2B000D),
+                    ),
+                    onPressed: () => context.read<LoginBloc>().add(Login()),
+                    child: const Text(
+                      'Sign In',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 28,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Don't have an account?",
+                    style: TextStyle(fontWeight: FontWeight.w500),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Sign Up',
+                      style: TextStyle(fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
+
           BlocSelector<LoginBloc, LoginState, bool>(
             selector: (state) => state.status == Status.loading,
             builder: (context, isLoading) {
@@ -95,7 +214,7 @@ class _LoginPageState extends State<LoginPage> {
                   child: const Center(child: CircularProgressIndicator()),
                 );
               }
-              return SizedBox.shrink();
+              return const SizedBox.shrink();
             },
           ),
         ],
