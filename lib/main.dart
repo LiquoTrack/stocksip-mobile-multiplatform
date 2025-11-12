3d2fb9f433ade14a61e4053145bf8300e08ff514
Far14z <faridce14@gmail.com>
2025-11-12 15:27:24 -0500

    feat(theme): implement MaterialTheme with light and dark color schemes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:stocksip/core/ui/theme.dart';
 import 'package:stocksip/features/iam/login/data/auth_service.dart';
 import 'package:stocksip/features/iam/login/presentation/pages/login_page.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stocksip/features/iam/login/presentation/blocs/login_bloc.dart';
-
 
 void main() {
   runApp(const MainApp());
@@ -14,14 +14,20 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
+
+    MaterialTheme theme = MaterialTheme(TextTheme());
+
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => LoginBloc(service: AuthService())),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Scaffold(body: LoginPage()),
+        theme: theme.light(),
+        darkTheme: theme.dark(),
+        home: Scaffold(          
+          body: LoginPage()
+        ),
       ),
     );
   }
