7b03f2adc3dd15ce2030ca0787eb63d7f98f4af2
Far14z <faridce14@gmail.com>
2025-11-11 20:28:59 -0500

    fix: change port of the api base url.

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
 
-  static final String baseUrl = 'http://localhost:5283/api/v1/';
+  static final String baseUrl = 'http://10.0.2.2:5283/api/v1/';
 
-  static final String signIn = '/sign-in';
+  static final String signIn = 'sign-in';
 }
