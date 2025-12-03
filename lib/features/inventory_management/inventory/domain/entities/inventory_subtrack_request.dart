1c7536c2fff12a5c9957da598ebd42eed75d27f7
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-12-02 22:34:38 -0500

    feat(inventories): add inventory subtrack request entity

---
+++ lib/features/inventory_management/inventory/domain/entities/inventory_subtrack_request.dart
@@ -0,0 +1,23 @@
+/// Represents a request to subtrack items from inventory.
+/// Includes the quantity to subtrack, the reason for subtracking, and an optional expiration date.
+class InventorySubtrackRequest {
+  final int quantityToSubtrack;
+  final String subtrackReason;
+  final DateTime? expirationDate;
+
+  /// Creates an [InventorySubtrackRequest] with the given parameters.
+  InventorySubtrackRequest({
+    required this.quantityToSubtrack,
+    required this.subtrackReason,
+    this.expirationDate,
+  });
+
+  /// Converts the [InventorySubtrackRequest] to a JSON representation.
+  Map<String, dynamic> toJson() {
+    return {
+      'quantityToDecrease': quantityToSubtrack,
+      'exitReason': subtrackReason,
+      'expirationDate': expirationDate?.toIso8601String(),
+    };
+  }
+}
